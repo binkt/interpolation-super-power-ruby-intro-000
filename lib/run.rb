5c7e826b1ef14213665943c138e471b6f2ceb039
Dennis Triplo <dennistriplo@yahoo.com>
2019-02-17 21:34:36 +0000

    Automatically backed up by Learn

--- lib/run.rb
+++ lib/run.rb
@@ -1,3 +1,3 @@
 require_relative './display_rainbow.rb'
 
-display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+display_rainbow(['red', 'orange', 'yellow', 'green', 'poop', 'indigo', 'violet'])
